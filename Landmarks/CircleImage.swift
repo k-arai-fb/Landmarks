e020a3e993a91d067ee9ab9d0ef0ad12be8ea091
k-arai-fb <k.arai@freebit.net>
2025-06-04 18:41:15 +0900

    Creating and combining views

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,18 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by k.arai on 2025/06/04.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
