6faa8600f187ada657aeab69e8a367d45e0f2f10
k-arai-fb <k.arai@freebit.net>
2025-06-05 14:41:50 +0900

    Finish HundlingUserInput

--- Landmarks/Views/Helper/MapView.swift
+++ Landmarks/Views/Helper/MapView.swift
@@ -8,19 +8,25 @@
 import SwiftUI
 import MapKit
 
+
 struct MapView: View {
+    var coordinate: CLLocationCoordinate2D
+    
+    
     var body: some View {
-        Map(initialPosition: .region(region))
+        Map(position: .constant(.region(region)))
     }
-    //構造体MKCoordinateResion()で初期化
+    
+    
     private var region: MKCoordinateRegion {
         MKCoordinateRegion(
-            center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
+            center: coordinate,
             span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
         )
     }
 }
 
+
 #Preview {
-    MapView()
+    MapView(coordinate: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868))
 }
---
+++ Landmarks/Views/Landmarks/LandmarkList.swift
@@ -0,0 +1,47 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by k.arai on 2025/06/05.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @Environment(ModelData.self) var modelData
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
+    var body: some View {
+        NavigationSplitView {
+            //id: \.idがあることで各要素を一意に識別 IdentifiableがあればOK
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .animation(.interactiveSpring, value: filteredLandmarks)
+            .navigationTitle("Landmarks")
+        } detail: {
+            Text("Select a landmark")
+        }
+        
+    }
+}
+
+#Preview {
+    LandmarkList()
+        .environment(ModelData())
+}
